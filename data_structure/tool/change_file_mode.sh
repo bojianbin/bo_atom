e7ca6265334306cb08ef383495929d284f2c7079
bojianbin <bojianbin1990@gmail.com>
2019-03-05 11:00:51 +0800

    change file mode

--- data_structure/tool/change_file_mode.sh
+++ data_structure/tool/change_file_mode.sh
@@ -2,24 +2,33 @@
 
 change()
 {
+    echo -e "\033[47;30m Enter dir $1 \033[0m"
+    
     for file in $1/*; do
+        if [ ! -e "$file" ]
+        then
+            echo "NOT find "$file
+            continue
+        fi
+
         if [ -f "$file" ] 
         then
             if [ "${file##*.}"x = "sh"x ] || [ "${file##*/}"x = "configure"x ]\
              || [ "${file##*-}"x = "sh"x ] || [ "${file##*/}"x = "INSTALL"x ]
             then
                 echo "SH FILE "$file
-                chmod 744 $file
+                chmod 744 "$file"
             else
                 echo "FILE "$file
-                chmod 644 $file
+                chmod 644 "$file"
             fi
         elif [ -d "$file" ] 
         then
             echo "DIR "$file
-            chmod 755 $file
-            change $file
+            chmod 755 "$file"
+            change "$file"
         fi
+
     done
 }
 
